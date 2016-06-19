9c71bbfc68d742a9495554b8e48a53780b861351
Oribe Abad <subcero000@gmail.com>
2016-06-19 02:39:48 -0500

    empezando con vistas dinamicas

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html', {})
-    
+    #posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    posts = Post.objects.all()
+    return render(request,'blog/post_list.html', {'posts':posts})
